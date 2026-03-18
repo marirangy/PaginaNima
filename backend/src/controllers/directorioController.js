092023fef529f03bfbf7f1c7f1e7a0e6ca289876
Mariana Rangel <rangelmariana60@gmail.com>
2026-03-17 19:58:54 -1200

    actualizar directorio

--- backend/src/controllers/directorioController.js
+++ backend/src/controllers/directorioController.js
@@ -2,9 +2,10 @@ import Directorio from '../models/Directorio.js';
 
 export const listarDirectorios = async (req, res) => {
   try {
-    const datos = await Centro.find();
+    const datos = await Directorio.find(); 
     res.json(datos);
   } catch (error) {
+    console.error("ERROR:", error);
     res.status(500).json({ error: "Error al obtener directorios" });
   }
 };
