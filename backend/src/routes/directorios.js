dccc2d81a56acd391f8cb5bbb82c6be17cb9758f
MarianaR <109435941+marirangy@users.noreply.github.com>
2026-03-27 23:35:48 -0600

    Update directorios.js

--- backend/src/routes/directorios.js
+++ backend/src/routes/directorios.js
@@ -1,12 +1,12 @@
 import express from "express";
-import Directorio from "../models/Directorio.js"; // asegúrate que esté
+import Directorio from "../models/Directorio.js"; 
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
   try {
     const directorios = await Directorio.find();
-    res.json(directorios); // ✅ AQUÍ va el cambio
+    res.json(directorios); // 
   } catch (error) {
     res.status(500).json({ error: "Error al obtener directorios" });
   }
